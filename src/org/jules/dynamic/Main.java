4f52093d560708498a0a6a00dad57f34c287d111
Jules95Game <romp.julian1@gmail.com>
2024-05-08 23:01:20 +0200

    Initial Main class

---
+++ src/org/jules/dynamic/Main.java
@@ -0,0 +1,11 @@
+package org.jules.dynamic;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Ready to start. Press enter... ");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+}
